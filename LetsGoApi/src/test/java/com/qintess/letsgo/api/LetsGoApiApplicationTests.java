b71a00c8d1b9a9349f1f4c143f6458edd3279c18
Plops013 <pecorafatec@gmail.com>
2020-05-13 20:05:38 -0300

    First Commit
    Primeiro commit, da api do LetsGo

---
+++ LetsGoApi/src/test/java/com/qintess/letsgo/api/LetsGoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.letsgo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
