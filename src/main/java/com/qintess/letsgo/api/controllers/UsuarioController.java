326f849e2746a72c2aee5285d344c8e3af016d31
Plops013 <pecorafatec@gmail.com>
2020-05-13 23:12:37 -0300

    adding cors

--- src/main/java/com/qintess/letsgo/api/controllers/UsuarioController.java
+++ src/main/java/com/qintess/letsgo/api/controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.qintess.letsgo.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import com.qintess.letsgo.api.models.Usuario;
 import com.qintess.letsgo.api.service.UsuarioService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/usuario")
 public class UsuarioController {
 
